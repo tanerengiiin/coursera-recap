c8fbff4280289353e58482531ca0119510194b96
tanerengiiin <tanerengin4444@gmail.com>
2023-10-12 16:11:32 +0300

    Add meta tags and navbar

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+const Header = () => {
+  return (
+    <header>
+        <img src={'/icons_assets/Logo.svg'} alt='Logo' id=''/>
+        <Nav/>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/reservation">Reservation</a></li>
+                <li><a href="/order">Order Online</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
